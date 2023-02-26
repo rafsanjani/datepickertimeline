4674c3e7b84262187daa77d54d2250f9ad212cc6
Rafsanjani Aziz <9197459+Rafsanjani@users.noreply.github.com>
2023-02-26 08:12:46 +0000

    Remove Unused Log import

--- datepickertimeline/src/main/java/com/foreverrafs/datepicker/DatePickerTimeline.kt
+++ datepickertimeline/src/main/java/com/foreverrafs/datepicker/DatePickerTimeline.kt
@@ -1,6 +1,5 @@
 package com.foreverrafs.datepicker
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
