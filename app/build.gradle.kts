d9130a368d2a0eefa2c8ff4a35ebee3c9d652e33
Rafsanjani Aziz <coded_raf@yahoo.com>
2024-11-21 08:13:31 +0000

    Fix build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,7 @@ android {
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
